d32f35bcff5f263950eb4d3e5288e916cb7ac2a1
Christo du Toit <christo@dutoit.co.uk>
2024-11-23 11:06:08 +0000

    ShouldMarkApiAsDeprecatedButNotExpired -> PASS

--- Attrify.DeprecatedApi.Tests.Unit/Middlewares/DeprecatedApiMiddlewareTests.Logic.cs
+++ Attrify.DeprecatedApi.Tests.Unit/Middlewares/DeprecatedApiMiddlewareTests.Logic.cs
@@ -2,8 +2,11 @@
 // Copyright (c)  Christo du Toit - All rights reserved.
 // -----------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
+using Attrify.Attributes;
 using Attrify.Middlewares;
+using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Moq;
@@ -55,6 +58,95 @@ namespace Attrify.InvisibleApi.Tests.Unit.Middlewares
 
             endpointFeatureMock.Verify(endpointFeature =>
                 endpointFeature.Endpoint,
+                    Times.Once);
+
+            requestDelegateMock.VerifyNoOtherCalls();
+            contextMock.VerifyNoOtherCalls();
+            featuresMock.VerifyNoOtherCalls();
+            endpointFeatureMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldMarkApiAsDeprecatedButNotExpired()
+        {
+            // given
+            var requestDelegateMock = new Mock<RequestDelegate>();
+            var contextMock = new Mock<HttpContext>();
+            var featuresMock = new Mock<IFeatureCollection>();
+            var endpointFeatureMock = new Mock<IEndpointFeature>();
+            var futureSunsetDate = DateTime.UtcNow.AddDays(30);
+
+            var deprecatedApiAttribute = new DeprecatedApiAttribute
+            {
+                Sunset = futureSunsetDate,
+                Warning = $"This API is deprecated and will be removed on {futureSunsetDate.ToString("yyyy-MM-dd")}.",
+                Link = "https://api.example.com/deprecation-info"
+            };
+
+            var endpoint = new Endpoint(
+                requestDelegate: null,
+                metadata: new EndpointMetadataCollection(deprecatedApiAttribute),
+                displayName: "TestEndpoint");
+
+            endpointFeatureMock.Setup(endpointFeature =>
+                endpointFeature.Endpoint)
+                .Returns(endpoint);
+
+            featuresMock.Setup(features =>
+                features.Get<IEndpointFeature>())
+                .Returns(endpointFeatureMock.Object);
+
+            contextMock.Setup(context =>
+                context.Features)
+                .Returns(featuresMock.Object);
+
+            var responseMock = new Mock<HttpResponse>();
+            var headersMock = new HeaderDictionary();
+            responseMock.Setup(response => response.Headers).Returns(headersMock);
+            contextMock.Setup(context => context.Response).Returns(responseMock.Object);
+
+            // when
+            var deprecatedApiMiddleware = new DeprecatedApiMiddleware(
+                next: requestDelegateMock.Object);
+
+            await deprecatedApiMiddleware.InvokeAsync(contextMock.Object);
+
+            // then
+            contextMock.Object.Response.Headers.ContainsKey("Sunset").Should().BeTrue();
+
+            contextMock.Object.Response.Headers["Sunset"].ToString()
+                .Should().Be(futureSunsetDate.ToString("yyyy-MM-dd"));
+
+            contextMock.Object.Response.Headers.ContainsKey("Warning").Should().BeTrue();
+
+            contextMock.Object.Response.Headers["Warning"].ToString()
+                .Should().Be($"{deprecatedApiAttribute.Warning}");
+
+            contextMock.Object.Response.Headers.ContainsKey("Link").Should().BeTrue();
+
+            contextMock.Object.Response.Headers["Link"].ToString()
+                .Should().Be($"<{deprecatedApiAttribute.Link}>; rel=\"deprecation\"");
+
+            contextMock.Object.Response.StatusCode.Should().NotBe(StatusCodes.Status410Gone);
+
+            requestDelegateMock.Verify(requestDelegate =>
+                requestDelegate(contextMock.Object),
+                    Times.Once);
+
+            featuresMock.Verify(features =>
+                features.Get<IEndpointFeature>(),
+                    Times.Once);
+
+            endpointFeatureMock.Verify(endpointFeature =>
+                endpointFeature.Endpoint,
+                    Times.Once);
+
+            contextMock.Verify(context =>
+                context.Response.Headers,
+                    Times.AtLeastOnce);
+
+            contextMock.Verify(context =>
+                context.Response.StatusCode,
                     Times.Once);
 
             requestDelegateMock.VerifyNoOtherCalls();
