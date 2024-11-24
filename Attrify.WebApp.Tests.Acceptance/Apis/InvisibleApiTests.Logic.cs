e26df802c8b6eb92d2b5e71810408dbba856346a
Christo du Toit <christo@dutoit.co.uk>
2024-11-24 17:28:46 +0000

    ShouldGetNotFoundForInvisibleWithNoHeadersOrRolesApiAsync -> PASS

--- Attrify.WebApp.Tests.Acceptance/Apis/InvisibleApiTests.Logic.cs
+++ Attrify.WebApp.Tests.Acceptance/Apis/InvisibleApiTests.Logic.cs
@@ -2,9 +2,12 @@
 // Copyright (c)  Christo du Toit - All rights reserved.
 // -----------------------------------------------------
 
+using System.Net;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Microsoft.AspNetCore.Http;
 
 namespace Attrify.WebApp.Tests.Acceptance.Apis
 {
@@ -32,5 +35,34 @@ namespace Attrify.WebApp.Tests.Acceptance.Apis
             bool hasWarningHeader = response.Headers.Contains("Warning");
             hasSunsetHeader.Should().BeFalse("because it should not include a 'Warning' header not deprecated.");
         }
+
+        [Fact]
+        public async Task ShouldGetNotFoundForInvisibleWithNoHeadersOrRolesApiAsync()
+        {
+            // given
+            var errorDetails = new
+            {
+                StatusCode = StatusCodes.Status404NotFound,
+                Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+                Title = $"{StatusCodes.Status404NotFound} -  Not Found",
+                Error = $"The requested resource could not be found. Please check the URL and try again.",
+            };
+
+            string expectedErrorMessage = JsonSerializer.Serialize(errorDetails, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+
+            // when
+            HttpResponseMessage response = await this.apiBroker.httpClient
+                .GetAsync(this.apiBroker.InvisibleRelativeUrl);
+
+            string actualErrorMessage = await response.Content.ReadAsStringAsync();
+
+            // then
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound, "because the API is invisible");
+            actualErrorMessage.Should().Be(expectedErrorMessage);
+        }
+
     }
 }
