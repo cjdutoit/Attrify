6b972db07a7971de78b71a3a378bd0ff0e468226
Christo du Toit <christo@dutoit.co.uk>
2024-11-24 16:04:54 +0000

    ShouldGetGoneStatusFromDeprecationApiAsync -> PASS

--- Attrify.WebApp.Tests.Acceptance/Brokers/ApiBroker.RelativeUrls.cs
+++ Attrify.WebApp.Tests.Acceptance/Brokers/ApiBroker.RelativeUrls.cs
@@ -9,7 +9,7 @@ namespace Attrify.WebApplication.Tests.Acceptance.Brokers
         private const string deprecatedApiRelativeBaseUrl = "api/deprecatedapi";
         public string NonDeprecatedRelativeUrl = $"{deprecatedApiRelativeBaseUrl}/active";
         public string PendingDeprecatedRelativeUrl = $"{deprecatedApiRelativeBaseUrl}/pending";
-        public string DeprecatedRelativeUrl = $"{deprecatedApiRelativeBaseUrl}/pending";
+        public string DeprecatedRelativeUrl = $"{deprecatedApiRelativeBaseUrl}/deprecated";
         private const string invisibleApiRelativeUrl = "api/invisibleapi";
         public string VisibleRelativeUrl = $"{invisibleApiRelativeUrl}/visible";
         public string InvisibleRelativeUrl = $"{invisibleApiRelativeUrl}/invisible";
