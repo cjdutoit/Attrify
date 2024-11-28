b81607bd4880d077d9ca6128c1cd0bd7dfd1cdde
Christo du Toit <christo@dutoit.co.uk>
2024-11-28 21:33:25 +0000

    ShouldReturnDeprecatedApisAsync -> PASS

--- Attrify.DeprecatedApi/Services/DeprecatedApiScanners/DeprecatedApiScannerService.cs
+++ Attrify.DeprecatedApi/Services/DeprecatedApiScanners/DeprecatedApiScannerService.cs
@@ -4,10 +4,16 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Attrify.Attributes;
 using Attrify.DeprecatedApi.Brokers.DateTimes;
 using Attrify.DeprecatedApi.Models.Foundations.DeprecatedApiScanners;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Routing;
 
 namespace Attrify.DeprecatedApi.Services.DeprecatedApiScanners
 {
@@ -22,7 +28,67 @@ namespace Attrify.DeprecatedApi.Services.DeprecatedApiScanners
             this.dateTimeBroker = dateTimeBroker;
         }
 
-        public async ValueTask<List<DeprecatedApiInfo>> GetDeprecatedApisAsync() =>
-            throw new NotImplementedException();
+        public async ValueTask<List<DeprecatedApiInfo>> GetDeprecatedApisAsync()
+        {
+            var deprecatedApis = new List<DeprecatedApiInfo>();
+            var controllerFeature = new ControllerFeature();
+            partManager.PopulateFeature(controllerFeature);
+
+            foreach (var controller in controllerFeature.Controllers)
+            {
+                var controllerName = controller.Name.Replace("Controller", "");
+                var controllerRoute = controller.GetCustomAttributes<RouteAttribute>()
+                    .FirstOrDefault()?.Template;
+
+                if (controllerRoute == null)
+                {
+                    controllerRoute = "defaultRoute";
+                }
+
+                var actions = controller
+                    .GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
+                        .Where(m => m.GetCustomAttributes<HttpMethodAttribute>().Any());
+
+                foreach (var action in actions)
+                {
+                    var deprecatedAttribute = action.GetCustomAttribute<DeprecatedApiAttribute>();
+
+                    if (deprecatedAttribute != null)
+                    {
+                        var httpMethods = action.GetCustomAttributes<HttpMethodAttribute>()
+                            .Select(a => a.HttpMethods.FirstOrDefault())
+                            .ToList();
+
+                        RouteAttribute actionRoute = action.GetCustomAttributes<RouteAttribute>()
+                            .FirstOrDefault();
+
+                        var methodAttribute = action.GetCustomAttributes<HttpMethodAttribute>()
+                            .FirstOrDefault();
+
+                        DateTimeOffset sunsetDateTimeOffset = DateTimeOffset.Parse(deprecatedAttribute.Sunset);
+                        DateTimeOffset currentDateTimeOffset = await dateTimeBroker.GetCurrentDateTimeOffsetAsync();
+
+                        // Ensure that both controllerRoute and actionRoute are handled correctly
+                        var route = controllerRoute != null && actionRoute != null
+                            ? $"{controllerRoute}/{actionRoute.Template}"
+                            : $"{controllerRoute}/{methodAttribute.Template}";
+
+                        deprecatedApis.Add(new DeprecatedApiInfo
+                        {
+                            Controller = controllerName,
+                            Action = action.Name,
+                            HttpMethods = httpMethods,
+                            Route = route,
+                            Sunset = deprecatedAttribute.Sunset,
+                            Warning = deprecatedAttribute.Warning,
+                            Link = deprecatedAttribute.Link,
+                            IsDeprecated = sunsetDateTimeOffset <= currentDateTimeOffset
+                        });
+                    }
+                }
+            }
+
+            return deprecatedApis;
+        }
     }
 }
